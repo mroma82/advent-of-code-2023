fbc7a6e5ad7a0addc8889076538ee19ee907d206
Michael Roma <mike@romacode.com>
2023-12-06 22:20:54 -0500

    day 3, part 1

--- 003/Program.cs
+++ 003/Program.cs
@@ -5,10 +5,9 @@ using System.Text.RegularExpressions;
 Console.WriteLine("Hello, World!");
 
 var partNumRegEx = new Regex("([0-9]+)");
+var symbolRegEx = new Regex(@"([\#\$\%\&\*/\+\-\=\@])");
 
-// define part numbers
-var partNumbers = new List<PartNumber>();
-var symbols = new List<PartNumber>();
+// parse part number
 PartNumber[] parsePartNumbers(string input, int row)
 {
     var matches = partNumRegEx.Matches(input);
@@ -25,35 +24,117 @@ PartNumber[] parsePartNumbers(string input, int row)
             Number = num,
             Row = row,
             StartIndex = grp.Index,
-            EndIndex = grp.Length
+            EndIndex = grp.Index + grp.Length - 1
         };
     }).ToArray();
-
 }
+
+// parse symbols
+PartNumber[] parseSymbols(string input, int row)
+{
+    var matches = symbolRegEx.Matches(input);
+
+
+    return matches.Select(x =>
+    {
+        var grp = x.Groups[1];
+
+        return new PartNumber
+        {
+            Symbol = grp.Value,
+            Row = row,
+            StartIndex = grp.Index
+        };
+    }).ToArray();
+}
+
+
+// define part numbers
+var partNumbers = new List<PartNumber>();
+var symbols = new List<PartNumber>();
 
 var chars = new List<char>();
 var fil = File.ReadLines("input.txt");
 int row = 1;
 foreach (var line in fil)
 {
-    Console.WriteLine(line);
+    //Console.WriteLine(line);
 
     var dis = line.ToCharArray().Distinct();
     chars.AddRange(dis.Where(x => !chars.Contains(x)));
 
     var _partNumbers = parsePartNumbers(line, row);
-    var x = String.Join(",", _partNumbers.Select(x => $"{x.Number}-{x.StartIndex}-{x.EndIndex}").ToList());
+    var _symbols = parseSymbols(line, row);
     partNumbers.AddRange(_partNumbers);
-    Console.WriteLine($" =====> {x}");
+    symbols.AddRange(_symbols);
+
+    //var x = String.Join(",", _partNumbers.Select(x => $"{x.Number}-{x.StartIndex}-{x.EndIndex}").ToList());
+    var x = String.Join(",", _symbols.Select(x => $"{x.Symbol}-{x.StartIndex}").ToList());
+    ////Console.WriteLine($" =====> {x}");
     row++;
 }
 
-chars.Sort();
-Console.WriteLine(chars.ToArray());
+// get possible
+(int, int)[] findPossible(PartNumber partNumber)
+{
+    // get the possible row, index values
+    var possible = new List<(int, int)>();
+
+    // above, below
+    for (var i = partNumber.StartIndex - 1; i <= partNumber.EndIndex + 1; i++)
+    {
+        possible.Add((partNumber.Row - 1, i));
+        possible.Add((partNumber.Row + 1, i));
+    }
+
+    // left, right
+    possible.Add((partNumber.Row, partNumber.StartIndex - 1));
+    possible.Add((partNumber.Row, partNumber.EndIndex + 1));
+
+    // return 
+    return possible.ToArray();
+}
+
+// go through parts
+var matchedParts = new List<int>();
+var matchedTotal = 0;
+foreach (var partNumber in partNumbers)
+{
+    // get the possible row, index values
+    var possible = findPossible(partNumber);
+
+    // show
+    /*
+    Console.WriteLine($"Checking possible: {partNumber.Number} at Row {partNumber.Row}, {partNumber.StartIndex}=>{partNumber.EndIndex}");
+    foreach ((int r, int i) in possible)
+    {
+        Console.WriteLine($" ==> {r}, {i}");
+    }*/
+
+    // check if a possible match
+    foreach ((int r, int i) in possible)
+    {
+        if (symbols.Any(x => x.Row == r && x.StartIndex == i))
+        {
+            Console.WriteLine(" ==> MATCH!");
+            matchedParts.Add(partNumber.Number);
+            matchedTotal += partNumber.Number;
+
+        }
+        else
+        {
+            Console.WriteLine(" ==> No MATCH!");
+        }
+    }
+}
+
+Console.WriteLine($"Day 3, Part 1: {matchedTotal}");
+
 
 class PartNumber
 {
     public int Number { get; set; }
+    public string Symbol { get; set; }
 
     public int Row { get; set; }
     public int StartIndex { get; set; }
